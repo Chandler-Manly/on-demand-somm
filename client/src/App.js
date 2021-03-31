98f5f20ea679fca5269e5032714eb7936f6414bb
HC <manlychandler@gmail.com>
2021-03-31 19:23:43 -0400

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
+// package imports
+import { Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+// import styling
 import './App.css';
+
+// component imports
 import Layout from './layouts/Layout'
+import Login from './screens/Login'
+
+// function imports 
+import {loginUser} from './services/auth'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    setCurrentUser(userData)
+  }
   return (
     <div className="App">
       <Layout>
-        <h3>Wine All Day</h3>
+        <Switch>
+          <Route path='/login'>
+            <Login
+              handleLogin={handleLogin}/>
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Layout(props) {
   return (
     <div>
       <header>
         <h1>ON-DEMAND-SOMM</h1>
+        <Link to='/login'>Login/Register</Link>
       </header>
       {props.children}
     </div>

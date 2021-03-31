c21240a663191627c6924c052553a8911fd11a27
HC <manlychandler@gmail.com>
2021-03-31 19:01:29 -0400

    debugged routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 post '/auth/login', to: 'authentication#login'
 get '/auth/verify', to: 'authentication#verify'
-resources :tastings, only :index
+resources :tastings, only: :index
 resources :wines
-resources :users, only: create
+resources :users, only: :create
 
 post '/wines/:wine_id/tastings/:id', to: 'tastings#add_tasting'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

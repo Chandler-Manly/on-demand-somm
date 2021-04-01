888dbccb415b81a75849e165f9312344d9401386
HC <manlychandler@gmail.com>
2021-03-31 21:20:06 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,70 @@
 // package imports
-import { Switch, Route } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
 
 // import styling
 import './App.css';
+import MainContainer from './containers/MainContainer';
 
 // component imports
-import Layout from './layouts/Layout'
-import Login from './screens/Login'
+import Layout from './layouts/Layout';
+import Login from './screens/Login';
+import Register from './screens/Register';
 
-// function imports 
-import {loginUser} from './services/auth'
+// function imports
+import { loginUser, registerUser, removeToken, verifyUser } from './services/auth';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    }
+    handleVerify();
+  }, [])
 
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData);
-    setCurrentUser(userData)
+    setCurrentUser(userData);
+    history.push('/');
   }
+
+  const handleRegister = async (formData) => {
+    const userData = await registerUser(formData);
+    setCurrentUser(userData);
+    history.push('/');
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+  }
+
   return (
     <div className="App">
-      <Layout>
+      <Layout
+        currentUser={currentUser}
+        handleLogout={handleLogout}
+      >
         <Switch>
           <Route path='/login'>
             <Login
-              handleLogin={handleLogin}/>
+              handleLogin={handleLogin}
+            />
+          </Route>
+          <Route path='/register'>
+            <Register
+              handleRegister={handleRegister}
+            />
+          </Route>
+          <Route path='/'>
+            <MainContainer
+              currentUser={currentUser}
+            />
           </Route>
         </Switch>
       </Layout>
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,13 +1,31 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Layout(props) {
+  const { currentUser, handleLogout } = props;
   return (
     <div>
       <header>
-        <h1>ON-DEMAND-SOMM</h1>
-        <Link to='/login'>Login/Register</Link>
+        <Link to='/'><h1>ON-DEMAND SOMM</h1></Link>
+        {
+          currentUser ?
+            <>
+              <p>{currentUser.username}</p>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+            :
+            <Link to='/login'>Login/Register</Link>
+        }
       </header>
+      <hr />
+      {currentUser && (
+        <>
+          <Link to='/wines'>Wines</Link>
+          <Link to='/tastings'>Tastings</Link>
+          <hr />
+        </>
+      )}
       {props.children}
     </div>
   )

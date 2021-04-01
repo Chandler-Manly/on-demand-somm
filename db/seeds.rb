e9b122850da13ff3016a0a662b6a0501ae709862
HC <manlychandler@gmail.com>
2021-03-31 21:20:32 -0400

    finished seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Wine.destroy_all
+Tasting.destroy_all
+User.destroy_all
 
 User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
 puts "#{User.count} users created"
 
-Tasting.create!(name:'')
+@salty = Tasting.create!(name: 'salty')
+@sweet = Tasting.create!(name: 'sweet')
+@sour = Tasting.create!(name: 'sour')
+@bitter = Tasting.create!(name: 'bitter')
+@umami = Tasting.create!(name: 'umami')
+
+puts "#{Tasting.count} Tasting created"
+
+Wine.create!(name: 'pizza', user: @admin, tastings: [@salty, @sweet, @umami])
+@ice_cream = Wine.create!(name: 'ice cream', user: @admin)
+
+@ice_cream.tastings.push(@sweet, @salty)
+
+@tacos = Wine.create!(name: 'tacos', user: @admin)
+
+@salty.wines << @tacos
+@sour.wines << @tacos
+@umami.wines << @tacos
+
+puts "#{Wine.count} wines created"

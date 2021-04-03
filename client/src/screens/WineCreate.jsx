c0038e3dc9d40550c569784cdffb2dd9757c615d
HC <manlychandler@gmail.com>
2021-04-03 15:35:32 -0400

    sourced WSET SAT approach

--- client/src/screens/WineCreate.jsx
+++ client/src/screens/WineCreate.jsx
@@ -1,20 +1,9 @@
+// Source for content credited to WSET Systematic Approach to Tasting (SAT)
+
 import { useState } from "react";
 import Parent from '../components/Parent';
 
 export default function WineCreate(props) {
-  // const [formData, setFormData] = useState({
-  //   name: ''
-  // })
-  // const { name } = formData;
-  // const { handleCreate } = props;
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormData(prevState => ({
-  //     ...prevState,
-  //     [name]: value
-  //   }))
-  // }
 
   return (
 

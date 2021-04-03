21154920a40a38fc9148789fa922b825defa0141
HC <manlychandler@gmail.com>
2021-04-03 13:48:09 -0400

    added tasting details to render on front end

--- client/src/screens/WineDetails.jsx
+++ client/src/screens/WineDetails.jsx
@@ -32,7 +32,40 @@ export default function WineDetails(props) {
     <div>
       <h3>{wineItem?.name}</h3>
       {wineItem?.tastings.map(tasting => (
-        <p key={tasting.id}>{tasting.name}</p>
+        <p key={tasting.id}>
+          <h1>{tasting.name}</h1>
+          <h3>Appearance</h3>
+          Clarity: {tasting.clarity}<br/>
+          Intensity: {tasting.intensity_aromatics}<br/>
+          Color: {tasting.color}<br/>
+          Other observations: {tasting.observations}<br/>          
+          
+          <h3>Nose</h3>
+          Condition: {tasting.condition} <br/>
+          Intensity: {tasting.intensity_palate}<br/>  
+          Aromas Primary: {tasting.aromas_primary} <br/>
+          Aromas Secondary: {tasting.aromas_secondary}<br/>
+          Aromas Tertiary: {tasting.aromas_tertiary} <br/>
+          Development: {tasting.development}
+          
+          <h3>Palate</h3>
+          Alcohol: {tasting.alcohol} <br/>
+          Sweetness: {tasting.sweetness} <br/>
+          Acidity: {tasting.acidity} <br/>
+          Tannin: {tasting.tannin} <br/>
+          Body: {tasting.body} <br/>
+          Mousse: {tasting.mousse} <br/>
+          Intensity: {tasting.intensity_flavor}<br/> 
+          Flavors Primary: {tasting.flavor_characteristics_primary}<br/>
+          Flavors Secondary: {tasting.flavor_characteristics_secondary}<br/>
+          Flavors Tertiary: {tasting.flavor_characteristics_tertiary}<br/>
+          Finish: {tasting.finish}
+
+          <h3>Conclusions</h3>
+          Quality Level: {tasting.quality_level}<br/>
+          Ageing Potential: {tasting.ageing_potential}
+        
+        </p>
       ))}
       <form onSubmit={(e) => {
         e.preventDefault()
